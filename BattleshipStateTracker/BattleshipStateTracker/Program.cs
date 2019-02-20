efaa30a19116b69cd8b11cc4a1a045ed3582dea2
bdsipon <sipon.here@gmail.com>
2019-02-20 14:11:19 +1000

    Created a project for the Battleship

---
+++ BattleshipStateTracker/BattleshipStateTracker/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BattleshipStateTracker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
