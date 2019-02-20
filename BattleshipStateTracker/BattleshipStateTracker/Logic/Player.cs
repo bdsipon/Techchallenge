3b9dd16efbb6f384b9660c1058bf693030ea4167
bdsipon <sipon.here@gmail.com>
2019-02-20 15:08:03 +1000

    added the logic

---
+++ BattleshipStateTracker/BattleshipStateTracker/Logic/Player.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BattleshipStateTracker.Logic
+{
+   public class Player
+    {
+        public String Name { get; set; }
+        public int Win { get; set; }
+       
+    }
+}
