6f9d7042a55ce4f23f36c615a219c1a4d5acd1a6
bdsipon <sipon.here@gmail.com>
2019-02-20 23:32:29 +1000

    added coordinate and shiprequest.

---
+++ BattleshipStateTracker/BattleshipStateTracker/Request/ShipRequest.cs
@@ -0,0 +1,14 @@
+﻿using BattleshipStateTracker.Ships;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BattleshipStateTracker.Request
+{
+  public class ShipRequest
+    {
+        public Coordinate Coordinate { get; set; }
+        public ShipDirection Direction { get; set; }
+        public ShipType ShipType { get; set; }
+    }
+}
